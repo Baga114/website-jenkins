ee85197d959f6d80770c39949d71ee6a4324e880
Dilbag Singh <113574343+Baga114@users.noreply.github.com>
2023-01-23 17:52:06 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install python3 -y
+sudo apt-get install docker.io -y
+sudo apt-get update
+sudo apt-get install docker.io -y 
+sudo apt install ca-certificates
+sudo apt-get install openjdk-11-jdk -y
