f3cce00d23d8dfc90db8c0e26d956068e88f21dc
Dilbag Singh <113574343+Baga114@users.noreply.github.com>
2023-01-25 19:39:27 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,5 +1,7 @@
 sudo su
 sudo apt-get update  
+sudo apt install ca-certificates
+sudo apt-get install openjdk-11-jdk -y
 sudo apt-get install docker.io -y
 service docker restart  
 sudo usermod -aG docker jenkins
